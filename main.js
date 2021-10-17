e979651e80e2b2a897f81a0dd972a892fb902faa
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-10-17 14:07:40 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var song = "";
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song.play();
+}
+
+function pause() {
+    song.pause();
+}
+
+function stop() {
+    song.stop();
+}
