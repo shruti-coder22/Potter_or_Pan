145fb8abef57beae1a470de4819e0affa27a223f
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-10-24 16:20:43 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var song = "";
 var left_y = 0;
 var right_y = 0;
 
+var left_x = 0;
+var right_x = 0; 
+
 function preload() {
     song = loadSound("music.mp3");
 }
@@ -22,9 +25,11 @@ function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
         
+        right_x = results[0].pose.rightWrist.x;
         right_y = results[0].pose.rightWrist.y;
         console.log("The y position of the right wrist is " + right_y);
 
+        left_x = results[0].pose.leftWrist.x;
         left_y = results[0].pose.leftWrist.y;
         console.log("The y position of the left wrist is " + left_y);
     }
@@ -36,6 +41,11 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000"); 
+    circle(left_x, left_y, 20);
+    circle(right_x, right_y, 20);
 }
 
 function play() {
